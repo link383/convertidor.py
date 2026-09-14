a7a5e4a8ea802d464ceb8134409118bd8fe4197c
link383 <vergeladan@gmail.com>
2026-09-14 10:54:20 -0400

    convertidor de monedas y grados

---
+++ conversor.py
@@ -0,0 +1,10 @@
+def celsius_to_fahrenheit(celsius):
+    """Convert Celsius to Fahrenheit."""
+    return (celsius * 9/5) + 32
+def fahrenheit_to_celsius(fahrenheit):
+    """Convert Fahrenheit to Celsius."""
+    return (fahrenheit - 32) * 5/9
+def dolar_to_bolivar(dolar):
+    """Convert Dolar to Bolivar."""
+    return dolar * 24.00
+
